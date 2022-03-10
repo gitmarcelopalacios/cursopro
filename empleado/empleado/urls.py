333dad12b9feab26be357e4174513d5a9c2bd2c7
Marcelo Carlos Palacios <correodemarcelopalacios@gmail.com>
2022-03-10 13:43:53 -0300

    Antes del Video 17

--- empleado/empleado/urls.py
+++ empleado/empleado/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+def PruebaUrl(self):
+    print("==== HOLA PROBANDO URLS ====")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('prueba/', PruebaUrl),
 ]
